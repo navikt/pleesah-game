e30ab49229e723d31c97bcde207e88aef9d08340
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2026-04-09 19:36:11 +0200

    Konverter siste side til nytt format

--- src/oppgaver/Ferdig.tsx
+++ src/oppgaver/Ferdig.tsx
@@ -1,17 +1,24 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Logo } from "../komponenter/logo/Logo.tsx";
 
 export const Ferdig = () => {
-    return (
-        <>
-            <h1>HURRA!!</h1>
-            <p>
-                Tusen takk for at du har spilt Pleesah Game! Vi håper du har hatt det gøy og lært masse om Kubernetes
-                underveis. Hvis du har noen tilbakemeldinger eller forslag til forbedringer, ikke nøl med å ta kontakt
-                med oss. Vi setter stor pris på din innsats og engasjement!
-            </p>
-            <p>Lykke til videre på din reise med Kubernetes, og måtte vinden alltid være i ditt seil!</p>
+    const naviagte = useNavigate();
 
-            <Link to={"/"}>Tilbake til forsiden</Link>
-        </>
+    return (
+        <main>
+            <div className="flex-column-container">
+                <Logo />
+                <h1 className="header">HURRA!!</h1>
+                <article>
+                    <p>
+                        Tusen takk for at du har spilt Pleesah Game! Vi håper du har hatt det gøy og lært masse om
+                        Kubernetes underveis. Hvis du har noen tilbakemeldinger eller forslag til forbedringer, ikke nøl
+                        med å ta kontakt med oss. Vi setter stor pris på din innsats og engasjement!
+                    </p>
+                    <p>Lykke til videre på din reise med Kubernetes, og måtte vinden alltid være i ditt seil!</p>
+                    <button onClick={() => naviagte("/")}>Tilbake til start</button>
+                </article>
+            </div>
+        </main>
     );
 };
