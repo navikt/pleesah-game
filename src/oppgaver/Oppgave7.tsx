f5d15d711cd280d3c6e1ad86a80ba287918a9bf5
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2026-04-09 19:29:45 +0200

    Oppgave 7 på plass

--- src/oppgaver/Oppgave7.tsx
+++ src/oppgaver/Oppgave7.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { Oppgaveside } from "../pages/Oppgaveside.tsx";
-import ReactMarkdown from "react-markdown";
-import oppgave7Tekst from "./oppgavetekster/oppgave7Tekst.md?raw";
+import { Logo } from "../komponenter/logo/Logo.tsx";
+import { useNavigate } from "react-router-dom";
 
 export const Oppgave7 = () => {
+    const navigate = useNavigate();
+
     const [serviceRunning, setServiceRunning] = useState(false);
 
     useEffect(() => {
@@ -32,11 +33,41 @@ export const Oppgave7 = () => {
     }, []);
 
     return (
-        <Oppgaveside
-            overskrift="Havnesjef"
-            oppgavetekst={<ReactMarkdown>{oppgave7Tekst}</ReactMarkdown>}
-            oppgavenummer={7}
-            nesteOppgaveDisabled={!serviceRunning}
-        />
+        <main>
+            <div className="flex-column-container">
+                <Logo />
+                <h1 className="header">Oppgave 7 - Havnesjef</h1>
+                <article>
+                    <p>
+                        Det er et stort hav og for å navigere deg videre ønsker du bistand fra flere flåter under en
+                        annen admiral, men med sterk tåke og mye sjø oppnår du ikke kontakt med andre flåter, og ingen
+                        får kontakt med deg! Hjelp! 📣
+                    </p>
+                    <p>
+                        For stabil og pålitelig kommunikasjon mellom deployments trenger vi en service. Vi liker å se på
+                        det som et radiotårn.
+                    </p>
+                    <pre>
+                        <code>{`apiVersion: v1
+kind: Service
+metadata:
+  name: DITT RADIOTÅRN
+spec:
+  selector:
+    app.kubernetes.io/name: NAVN PÅ ADMIRALEN?
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 8080`}</code>
+                    </pre>
+                    <div className="horizontal-button-container" style={{ marginTop: "2rem" }}>
+                        <button onClick={() => navigate("/oppgaver/6/")}>{"<-- Forrige oppgave!"}</button>
+                        <button disabled={!serviceRunning} onClick={() => navigate("/ferdig/")}>
+                            {`Ferdig ${serviceRunning ? "✅" : "⏳"}`}
+                        </button>
+                    </div>
+                </article>
+            </div>
+        </main>
     );
 };
