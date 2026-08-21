a014527948b358034cf42b37502e7b858dbbf060
Kyrre Havik <kyrre.havik@nav.no>
2026-08-21 14:41:14 +0200

    💬 gått igjennom oppgave 0

--- src/sider/oppgaver/Oppgave0.tsx
+++ src/sider/oppgaver/Oppgave0.tsx
@@ -17,34 +17,66 @@ export const Oppgave0 = () => {
     <main>
       <Header
         overskrift={lagOppgaveoverskrift(OPPGAVENUMMER, "Kubectl")}
-					begreper={[Begrep.Namespace, Begrep.Pod]}
+        begreper={[Begrep.Namespace, Begrep.Pod]}
         kommandoer={[KubectlKommando.Help, KubectlKommando.Describe]}
       />
       <div className="flex-column-container">
         <article>
           <Historiecontainer>
+            Dere er nå klar til å starte deres reise som pirat! Dere rusler ned
+            mot havna for å se etter et skip. Der finner dere skipet Den Sorte
+            Perle, og som de ekte piratene dere er, kaprer du dette skipet!
             Ombord på Den Sorte Perle må dere inspisere at den er klart til å
             sette seil på de syv hav!
           </Historiecontainer>
 
           <p>
-            <code>kubectl</code> er hovedverktøyet når man jobber med
-            Kubernetes. Den lar dere enkelt se og interagere med alle ressursene
-            som finnes. Derfor starter vi med en enkel oppgave hvor dere skal se
-            at det ikke finnes noen podder kjørende i vårt{" "}
-            <Tooltip begrep={Begrep.Namespace} />. Som nevnt tidligere brukes
-            namespace for å holde ressurser adskilt. Dette gjør at man enkelt
-            kan styre rettigheter, tilganger, og kommunikasjon på tvers av
-            avhengigheter. I Pleesah skiller vi mellom de forskjellige teamene,
-            slik at dere ikke går i beina på hverandre.
+            <code>kubectl</code> er et kommandolinjeverktøy, og er
+            hovedverktøyet når man jobber med Kubernetes. Kubetctl lar dere
+            enkelt se og interagere med alle ressursene som finnes i et cluster.
+            Derfor starter vi med en enkel oppgave hvor dere skal se om dere får
+            kontakt med deres <Tooltip begrep={Begrep.Namespace} />, og
+            interagere med en av de mest brukte ressurstypene i Kubernetes,
+            nemlig <Tooltip begrep={Begrep.Pod} />.
           </p>
-
           <p>
-            <code>kubectl</code> er Kubernetes sitt eget kommandolinjeverktøy
-            for å kommunisere med et Kubernetes cluster.
+            Som nevnt tidligere brukes namespace for å holde ressurser adskilt.
+            Dette gjør at man enkelt kan holde ressurser avskilt, og styre
+            rettigheter, tilganger, og kommunikasjon på tvers av namespaces. I
+            Pleesah skiller vi mellom de forskjellige teamene, slik at dere ikke
+            går i beina på hverandre. Så derfor vil du ikke kunne se hva de
+            andre holder på med.
           </p>
 
           <code>kubectl [KOMMANDO] [RESSURSTYPE] [RESSURSNAVN] [FLAGG]</code>
+
+          <p>
+            <code>kubectl</code> er en samling med kommandoer/verb som kan
+            interagere med de mange ressurstypene som finnes i Kubernetes. De to
+            vanligste kommandoene er <Tooltip begrep={KubectlKommando.Get} /> og{" "}
+            <Tooltip begrep={KubectlKommando.Describe} />. Hvor{" "}
+            <Tooltip begrep={KubectlKommando.Get} /> lister opp en eller flere
+            ressurser, mens <Tooltip begrep={KubectlKommando.Describe} /> viser
+            en detaljert beskrivelse av en ressurs.
+          </p>
+
+          <p>
+            Oppgaven går da ut på å liste opp alle{" "}
+            <Tooltip begrep={Begrep.Pod} value="podder" />, siden spillet
+            nettopp har begynt vil det ikke være noe å liste op, og dere vil få
+            følgende melding i terminalen deres:
+          </p>
+
+          <pre>
+            <code>
+              No resources found in {localStorage.getItem("team")} namespace
+            </code>
+          </pre>
+
+          <p>
+            Hvis dere får samme respons som over, har dere gjort det riktig! Nå
+            kan dere gå videre til neste oppgave.
+          </p>
 
           <h2>Tips og triks</h2>
           <ul>
@@ -58,22 +90,6 @@ export const Oppgave0 = () => {
               kan du bruke <code>kubectl describe</code>
             </li>
           </ul>
-
-          <p>
-            For å sikre at alt er riktig, så må dere sjekke at det ikke allerede
-            finnes en <Tooltip begrep={Begrep.Pod} /> i deres namespace.
-          </p>
-
-          <pre>
-            <code>
-              No resources found in {localStorage.getItem("team")} namespace
-            </code>
-          </pre>
-
-          <p>
-            Hvis dere får samme respons som over har dere gjort det riktig! Nå
-            er dere klare til å sjøsette skuta!
-          </p>
 
           <div className="hint-button-container">
             <button onClick={() => setVisHint1(true)}>Hint 1</button>
