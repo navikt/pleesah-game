b5848d625461300be82798af139681a63ae0b4e4
Kyrre Havik <kyrre.havik@nav.no>
2026-08-25 13:27:14 +0200

    💬 gått igjennom ferdig

--- src/sider/oppgaver/Ferdig.tsx
+++ src/sider/oppgaver/Ferdig.tsx
@@ -8,21 +8,17 @@ export const Ferdig = () => {
     <main>
       <div className="flex-column-container">
         <Header overskrift="HURRA!" />
-        {/*<article>*/}
-        {/*  <TeamStatus />*/}
-        {/*</article>*/}
         <article>
           <p>
             Tusen takk for at du har spilt Pleesah Game! Vi håper du har hatt
             det gøy og lært masse om Kubernetes underveis. Hvis du har noen
-            tilbakemeldinger eller forslag til forbedringer, ikke nøl med å ta
-            kontakt med oss. Vi setter stor pris på deres innsats og
-            engasjement!
+            tilbakemeldinger, idéer til oppgaver, eller forslag til
+            forbedringer, ikke nøl med å ta kontakt med oss eller fyll ut
+            skjemaet vi lenker til nednefor. Vi setter stor pris på deres
+            innsats og engasjement!
           </p>
 
           <p>
-            Vi hadde ikke satt veldig stor pris på tilbakemeldingene deres!
-            <br />
             <a
               href="https://forms.cloud.microsoft/e/EhzrZFsKfz"
               target="_blank"
